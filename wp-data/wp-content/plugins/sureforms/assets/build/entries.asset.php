7b78aa9caff291a746af3cff12feeb654d09756b
Elesar De La Vega <elesarvega@gmail.com>
2025-12-05 11:00:32 +0100

    feat: :zap: add new fotos, plugins and uploads

---
+++ wp-data/wp-content/plugins/sureforms/assets/build/entries.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'b29d01c2db8848143138');
---
+++ wp-data/wp-content/plugins/sureforms/assets/build/forms.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '0289bd92da11c48ebba8');
