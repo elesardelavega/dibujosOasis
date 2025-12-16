d49e3c3648446a502af8254e80883489b1c3475b
Elesar De La Vega <elesarvega@gmail.com>
2025-12-16 11:38:53 +0100

    feat: :art: add uploads, themes ...

---
+++ wp-data/wp-content/uploads/uag-plugin/assets/1000/uag-js-1201.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-peqc64fd' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-84f0a274' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e999a5e7' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8e55d724' );
+});
+ });
